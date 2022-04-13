72e7d5974a40c85db9f49f4bad1750ecdcf967af
Elena <MissKatharsis@yandex.ru>
2022-04-13 10:19:59 +0300

    fix menu cli.py and brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 from brain_games import cli
 
-
-def greet():
-    print('Welcome to the Brain Games!')
-    
+  
 def main():
-    greet()
     cli.welcome_user()
-    
+
 if __name__ == '__main__':
     main()
