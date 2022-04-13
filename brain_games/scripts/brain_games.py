f0e2837bd77c1ff8c59e145fb53834e0caad511d
Elena <MissKatharsis@yandex.ru>
2022-04-13 10:15:28 +0300

    add import from cli.py to brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+from brain_games import cli
+
+
 def greet():
     print('Welcome to the Brain Games!')
     
 def main():
     greet()
+    cli.welcome_user()
     
 if __name__ == '__main__':
     main()
