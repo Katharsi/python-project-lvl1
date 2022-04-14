cc00c39803bc8b6baea161d4d12f51bb4c512048
Elena <MissKatharsis@yandex.ru>
2022-04-14 18:46:53 +0300

    create new function to cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,10 @@
 import prompt
 
 
-def welcome_user():
+def get_welcome_username():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+    
+def get_answer_user():
+    answer = prompt.string('Your answer: ')
