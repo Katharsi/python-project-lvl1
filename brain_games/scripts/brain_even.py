719cb565d6dc66f2f8ee44bdf1b0a6dde42ffba0
Elena <MissKatharsis@yandex.ru>
2022-04-14 19:15:59 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,10 @@
 from brain_games import cli
 
 
-print('Answer "yes" if the number is even, otherwise answer "no".')
-
 def main():
     cli.get_welcome_username()
 
+print('Answer "yes" if the number is even, otherwise answer "no".')
 
 if __name__ == '__main__':
     main()
