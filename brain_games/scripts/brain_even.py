0b04fee674ee9782d227cef95b873a60e4225300
Elena <MissKatharsis@yandex.ru>
2022-04-14 19:11:34 +0300

    new function to brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+from brain_games import cli
+
+
+print('Answer "yes" if the number is even, otherwise answer "no".')
+
+def main():
+    cli.get_welcome_username()
+
+
+if __name__ == '__main__':
+    main()
