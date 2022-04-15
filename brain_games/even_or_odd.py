1c068215f7e5b001ec2dcf2676abfe88394e3f13
Elena <MissKatharsis@yandex.ru>
2022-04-15 13:44:43 +0300

    change function for even_or_odd.py

--- brain_games/even_or_odd.py
+++ brain_games/even_or_odd.py
@@ -5,28 +5,29 @@ from random import randint
 
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 NUMBER_OF_ROUND = 3
+start_round = 1
 
-def generate_number():
-    number = randint(1, 99)
-    answer = 'yes' if number % 2 is True else 'no'
-    return str(number), answer
-
-
-def get_game(game):
-    print('Welcome to the Brain Games!')
+def get_game():
+    print('Welcome to the Brain Games!')  
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+
     print(DESCRIPTION)
-    start_round = 1
-
-    for start_round in range(NUMBER_OF_ROUND):
-        question, answer = game.generate_number()
-        print(f'Question: {question}')
-
-        user_answer = prompt.string('Your answer: ')
-        if str(user_answer) == str(answer):
-            print('Correct!')
-        else:
-            print(f"{user_answer} is wrong answer ;(. Correct anwer was {answer}."
-                  f"Let's try again, {name}!")
+    for start_round in range(NUMBER_OF_ROUND): 
+        number = randint(1, 99)
+        print(f'Question: {number}')
+    
+        answer = prompt.string('Your answer: ')
+        if answer == 'yes':
+            if number % 2 == 0:
+                print('Correct!')
+            elif number % 2 != 0:
+                print(f"'yes' is wrong answer ;(. Correct answer was 'no'."
+                      f"Let's try again, {name}!")
+        elif answer == 'no':
+            if number % 2 != 0:
+                print('Correct!')
+            elif number % 2 == 0:
+                print(f"'no' is wrong answer ;(. Correct anwer was 'yes'."
+                      f"Let's try again, {name}!")
     print(f'Congratulations, {name}!')
