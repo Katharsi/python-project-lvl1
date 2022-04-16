4d5afcca9f717434b6fcc3a412208f734a78bb03
Elena <MissKatharsis@yandex.ru>
2022-04-16 18:20:41 +0300

    rewrite new way for script brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-from brain_games import even_or_odd
+from brain_games.base_brain import run_game
+from brain_games.games import brain_even
 
 
 def main():
-    even_or_odd.get_game()   
+    """Run main function."""
+    run_game(brain_even)
 
 
 if __name__ == '__main__':
