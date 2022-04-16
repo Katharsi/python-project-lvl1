f0a340a1210ec5809415466e0c518714ed2e0a7a
Elena <MissKatharsis@yandex.ru>
2022-04-16 18:21:31 +0300

    rewrite even_or_odd.py to games/brain_even.py

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,21 @@
+"""brain-even game."""
+from random import randint
+
+
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def is_even(number):
+    """
+    Checking the number for parity.
+    Boolean type - True or False.
+    """
+    
+    return number % 2 == 0
+
+
+def generate_number():
+    """Creating a question-answer form."""
+
+    game_question = randint(1, 99)
+    correct_answer = 'yes' if is_even(game_question) else 'no'
+    return game_question, correct_answer
