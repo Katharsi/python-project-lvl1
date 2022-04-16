f33a51c4dd250e312c7c1cb022de656209d8354e
Elena <MissKatharsis@yandex.ru>
2022-04-16 18:38:47 +0300

    fix base_brain.py

--- brain_games/base_brain.py
+++ brain_games/base_brain.py
@@ -11,13 +11,6 @@ def welcome_user():
     return username
 
 
-def get_answer_user():
-    """Getting an answer to the question posed."""
-
-    user_answer = prompt.string('Your answer: ')
-    return user_answer
-
-
 def run_game(game, game_rounds=3):
     """Run base menu game."""
 
@@ -26,13 +19,13 @@ def run_game(game, game_rounds=3):
     while game_rounds:
         game_question, correct_answer = game.generate_number()
         print(f'Question: {game_question}')
-        user_answer = get_answer_user()
+        user_answer = prompt.string('Your answer: ')
         if user_answer == correct_answer:
             print('Correct!')
             game_rounds -= 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'"/
-                  f"Let's try again, {username}!")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'")
+            print(f"Let's try again, {username}!")
             break
     if not game_rounds:
         print(f'Congratulations, {username}!')
