ea03f0784d35c0f170d7813f2466a38f2ee373ce
Elena <MissKatharsis@yandex.ru>
2022-04-17 11:20:24 +0300

    create new game brain_calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,39 +1,22 @@
-import prompt
+"""brain-calc game."""
+from random import choice, randint
 
-from random import randint, choice
 
-
-ROUNDS = 3
 DESCRIPTION = 'What is the result of the expression?'
 OPERATOR = ['+', '-', '*']
 
 
-def calc(number_one, operators, number_two):
-    if operators == '+':
-        return number_one + number_two
-    if operators == '-':
-        return number_one - number_two
-    if operators == '*':
-        return number_one * number_two
-
-
-def generate_operation():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print(DESCRIPTION)
-    for start in range(ROUNDS):
-        number_one = randint(1, 99)
-        number_two = randint(1,99)
-        operators = choice(OPERATOR)
-        question = f'{number_one} {operators} {number_two}'
-        print(f'Question: {question}')
-        answer = calc(number_one, operators, number_two)
-        user_answer = prompt.string('Your answer: ')
-        if user_answer:
-            if answer == user_answer:
-                print('Correct!')
-            else:
-                print(f"{str(user_answer)} is wrong answer ;(. Correct answer was {str(answer)}.")
-                print(f"Let's try again, {name}!")
-    print(f'Congratulations, {name}!')
+def generate_number_equation():
+    """Generate math random number and operator."""
+    number_one = randint(1, 99)
+    number_two = randint(1, 99)
+    operator = choice(OPERATOR)
+    game_question = f'{number_one} {operator} {number_two}'
+    if operator == '+':
+        correct_answer = str(number_one + number_two)
+        return game_question, correct_answer
+    elif operator == '-':
+        correct_answer = str(number_one - number_two)
+        return game_question, correct_answer
+    correct_answer = str(number_one * number_two)
+    return game_question, correct_answer
