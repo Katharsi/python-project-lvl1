272ff082e3738f720ab71db23098d1c1e65a1f3e
Elena <MissKatharsis@yandex.ru>
2022-04-17 11:20:55 +0300

    create new script for run game brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+from brain_games.base_brain import run_game
 from brain_games.games import brain_calc
 
 
 def main():
-    brain_calc.generate_operation()   
+    """Run main function."""
+    run_game(brain_calc)
 
 
 if __name__ == '__main__':
