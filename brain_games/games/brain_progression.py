5a4314c6735b9a4e6fa12e9a4b27a54f9276ce2c
Elena <MissKatharsis@yandex.ru>
2022-04-19 23:15:00 +0300

    refactoring brain-progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -7,23 +7,16 @@ DESCRIPTION = 'What number is missing in the progression?'
 
 def get_progression(start_number, step_progression, length_progression):
     """Generating an arithmetic progression with a random skip."""
-    result = [start_number]
-    i = 0
-    while i < length_progression:
-        start_number += step_progression
-        result.append(start_number)
-        i += 1
-    return result
+    return list(range(start_number, start_number + length_progression * step_progression, step_progression))
 
 
 def generate_number():
     """Generating an arithmetic progression with a random skip."""
     start_number = randint(1, 99)
     step_progression = randint(1, 99)
-    length_progression = randint(1, 10)
-    func_progression = get_progression(start_number, step_progression, length_progression)
-    index = randint(0, length_progression)
-    correct_answer = func_progression[index]
-    func_progression[index] = '..'
-    game_question = ' '.join(map(str, func_progression))
-    return game_question, correct_answer
+    length_progression = 10
+    func_progression = [str(num) for num in get_progression(start_number, step_progression, length_progression)]
+    missing_num = randint(0, len(func_progression) - 1)
+    correct_answer = func_progression[missing_num]
+    func_progression[missing_num] = '..'
+    return ' '.join(func_progression), correct_answer
