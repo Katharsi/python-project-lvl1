45ea88d7aa3d615573a82ec0ce402fc18b2c5346
Elena <MissKatharsis@yandex.ru>
2022-04-19 23:23:21 +0300

    delete long character

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -7,7 +7,9 @@ DESCRIPTION = 'What number is missing in the progression?'
 
 def get_progression(start_number, step_progression, length_progression):
     """Generating an arithmetic progression with a random skip."""
-    return list(range(start_number, start_number + length_progression * step_progression, step_progression))
+    return list(range(start_number,
+                      start_number + length_progression * step_progression,
+                      step_progression))
 
 
 def generate_number():
@@ -15,7 +17,9 @@ def generate_number():
     start_number = randint(1, 99)
     step_progression = randint(1, 99)
     length_progression = 10
-    func_progression = [str(num) for num in get_progression(start_number, step_progression, length_progression)]
+    func_progression = [str(num) for num in get_progression(start_number,
+                                                            step_progression,
+                                                            length_progression)]
     missing_num = randint(0, len(func_progression) - 1)
     correct_answer = func_progression[missing_num]
     func_progression[missing_num] = '..'
