b2451c7c5ce01f5f779584b3bc62e8f8beb15bda
Elena <MissKatharsis@yandex.ru>
2022-05-02 11:35:47 +0300

    refactoring brain_calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,22 +1,28 @@
 """brain-calc game."""
+import operator
+
 from random import choice, randint
 
 
 DESCRIPTION = 'What is the result of the expression?'
-OPERATOR = ['+', '-', '*']
 
 
-def generate_number():
+operators = {'+': operator.add,
+             '-': operator.sub,
+             '*': operator.mul,
+}
+
+
+def get_random_number():
     """Generate math random number and operator."""
-    number_one = randint(1, 99)
-    number_two = randint(1, 99)
-    operator = choice(OPERATOR)
+    number_one = randint(0, 100)
+    number_two = randint(1, 100)
+    operator = choice(list(operators.keys()))
     game_question = f'{number_one} {operator} {number_two}'
-    if operator == '+':
-        correct_answer = str(number_one + number_two)
-        return game_question, correct_answer
-    elif operator == '-':
-        correct_answer = str(number_one - number_two)
-        return game_question, correct_answer
-    correct_answer = str(number_one * number_two)
+    correct_answer = get_correct_answer(number_one, operator, number_two)
     return game_question, correct_answer
+
+
+def get_correct_answer(number_one, operator, number_two):
+    """Get programm correct_answer."""
+    return str(operators[operator](number_one, number_two))
