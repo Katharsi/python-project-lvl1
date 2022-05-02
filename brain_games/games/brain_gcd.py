17147657f0bd45db5e1f3ea94a5d2684eb6e972c
Elena <MissKatharsis@yandex.ru>
2022-05-02 13:05:24 +0300

    refactoring brain_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -7,8 +7,8 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 def generate_number():
     """Generate random numbers and find the largest divisor."""
-    number_one = randint(1, 99)
-    number_two = randint(1, 99)
+    number_one = randint(1, 100)
+    number_two = randint(1, 100)
     game_question = f'{number_one} {number_two}'
     correct_answer = gcd(number_one, number_two)
     return game_question, str(correct_answer)
@@ -16,7 +16,7 @@ def generate_number():
 
 def gcd(number_one, number_two):
     """Generate GCD function, using the recursion."""
-    if(number_two == 0):
+    if number_two == 0:
         return number_one
     else:
         return gcd(number_two, number_one % number_two)
