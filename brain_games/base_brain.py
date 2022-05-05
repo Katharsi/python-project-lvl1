75e148fc24fe11a78006e78edff7df9f424ce802
Elena <MissKatharsis@yandex.ru>
2022-05-05 15:03:30 +0300

    add return username for welcome_user function

--- brain_games/base_brain.py
+++ brain_games/base_brain.py
@@ -7,6 +7,7 @@ def welcome_user():
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
     print(f'Hello, {username}!')
+    return username
 
 
 def run_game(game, game_rounds=3):
